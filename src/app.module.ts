596844fab4e6fa40db7d5f95fc5119b56a9dbd65
tthiagonobre <thiagocardoso.nobre@gmail.com>
2025-11-20 17:58:24 -0400

    feat: setup core modules for whatsapp, schedulling, and database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { AgendamentoModule } from './agendamento/agendamento.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [WhatsappModule, AgendamentoModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
